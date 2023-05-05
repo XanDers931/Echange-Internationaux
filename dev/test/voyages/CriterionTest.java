c7f981a4f381f1374a2c935081fb4e98900937eb
Paul Degraeve <paul.degraeve.etu@univ-lille.fr>
2023-05-05 15:03:16 +0200

    correction cirtiterionTest

--- dev/test/voyages/CriterionTest.java
+++ dev/test/voyages/CriterionTest.java
@@ -1,16 +1,8 @@
 package voyages;
 
-import voyages.Criterion;
-import voyages.CriterionName;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import javax.accessibility.AccessibleAttributeSequence;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CriterionTest {
