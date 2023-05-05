85be629c5d5ba5d0fc97c1d7f68331f56ba46f07
Paul <paul1.degraeve@gmail.com>
2023-05-05 18:30:57 +0200

    ajout javadoc pour countryname

--- dev/src/voyages/CountryName.java
+++ dev/src/voyages/CountryName.java
@@ -1,6 +1,11 @@
 package voyages;
 
+/** The country enumeration. It contains the countries in which teenagers live.
+ * @author Dagneaux Nicolas
+ * @author Degraeve Paul
+ * @author Martel Alexandre
+ * @see Teenager
+ */
 public enum CountryName {
     FRANCE, ITALY, SPAIN, GERMANY
-       
 }
