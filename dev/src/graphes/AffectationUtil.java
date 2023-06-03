865c5e7f2f79d2dbca60f4d6d7eeba7e70a943f3
Paul <paul1.degraeve@gmail.com>
2023-06-03 11:50:24 +0200

    graph gestion de l'historique

--- dev/src/graphes/AffectationUtil.java
+++ dev/src/graphes/AffectationUtil.java
@@ -1,5 +1,7 @@
 package graphes;
 
+import java.util.List;
+
 import voyages.*;
 
 /**The {@code AffectationUtil} class, it represents a utility class for the affectation problem.
@@ -10,18 +12,31 @@ import voyages.*;
  * @see Teenager
  */
 public class AffectationUtil {
+
+	static final int DEFAULT_WEIGHT = 1;
 	
 	/** This method return the weight of the edge between the host and the visitor, based on their affinity.
 	 * @param host the {@code Teenager} host.
 	 * @param visitor the {@code Teenager} visitor.
 	 * @return a double that represents the weight of the edge.
 	 */
-	public static double weight (Teenager host, Teenager visitor) {
-		double weight = host.compatibleWithGuest(visitor) ? 1 : Double.MAX_VALUE;
-		if (weight == 1) {
+	public static double weight (Teenager host, Teenager visitor, List<Tuple<Teenager>> history) {
+		double weight = host.compatibleWithGuest(visitor) ? DEFAULT_WEIGHT : Double.MAX_VALUE;
+		if (weight == DEFAULT_WEIGHT) {
+			weight += historyWeight(host, visitor, history);
 			weight += affinityHobbies(host, visitor);
 		}
 		return weight;
+	}
+
+	private static double historyWeight(Teenager host, Teenager visitor, List<Tuple<Teenager>> history) {
+		if (host.getRequirement().get(CriterionName.HISTORY).getValue().equals("other") || visitor.getRequirement().get(CriterionName.HISTORY).getValue().equals("other")) {
+			return Double.MAX_VALUE;
+		} else if (host.getRequirement().get(CriterionName.HISTORY).getValue().equals("same") && visitor.getRequirement().get(CriterionName.HISTORY).getValue().equals("same")) {
+			return -1;
+		} else {
+			return -0.1;
+		}
 	}
 
 	/** This method return a double representing the affinity between the host and the visitor, based on their hobbies.
