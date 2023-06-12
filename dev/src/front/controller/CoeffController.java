229d09b209e75f99d20d41554438d687b6e62047
Paul <paul1.degraeve@gmail.com>
2023-06-12 22:49:05 +0200

    gestion des coeff ok (normalement)

--- dev/src/front/controller/CoeffController.java
+++ dev/src/front/controller/CoeffController.java
@@ -1,14 +1,28 @@
 package front.controller;
 
 import front.FXMLScene;
+import graphes.AffectationUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class CoeffController extends SceneController {
 
     @FXML private Button quitbutton;
+
+    @FXML private TextField coeffAnimal;
+
+    @FXML private TextField coeffDiet;
+
+    @FXML private TextField coeffAge;
+
+    @FXML private TextField coeffSexe;
+
+    @FXML private TextField coeffHistory;
+
+    @FXML private TextField coeffHobbies;
     
     public CoeffController(Stage stage) {
         super(FXMLScene.COEFF_MANAGER.getPath(), FXMLScene.COEFF_MANAGER.getTitle(), stage);
@@ -18,7 +32,46 @@ public class CoeffController extends SceneController {
     @Override
     public void initialize() {
         this.quitbutton.addEventHandler(ActionEvent.ACTION, a -> {
+            if (!this.coeffAnimal.getText().equals("")) {
+                AffectationUtil.setCoefficientAnimalAllergy(Integer.parseInt(this.coeffAnimal.getText()));
+            }
+            if (!this.coeffDiet.getText().equals("")) {
+                AffectationUtil.setCoefficientDiet(Integer.parseInt(this.coeffDiet.getText()));
+            }
+            if (!this.coeffAge.getText().equals("")) {
+                AffectationUtil.setCoefficientAge(Integer.parseInt(this.coeffAge.getText()));
+            }
+            if (!this.coeffSexe.getText().equals("")) {
+                AffectationUtil.setCoefficientGender(Integer.parseInt(this.coeffSexe.getText()));
+            }
+            if (!this.coeffHistory.getText().equals("")) {
+                AffectationUtil.setCoefficientHistory(Integer.parseInt(this.coeffHistory.getText()));
+            }
+            if (!this.coeffHobbies.getText().equals("")) {
+                AffectationUtil.setCoefficientHobbies(Integer.parseInt(this.coeffHobbies.getText()));
+            }
             this.stage.close();
         });
+    }
+
+    public void numberTest() {
+        if (!this.coeffAnimal.getText().matches("^[1-9]{0,1}+$|^$")) {
+            this.coeffAnimal.setText("");
+        }
+        if (!this.coeffDiet.getText().matches("^[1-9]{0,1}+$|^$")) {
+            this.coeffDiet.setText("");
+        }
+        if (!this.coeffAge.getText().matches("^[1-9]{0,1}+$|^$")) {
+            this.coeffAge.setText("");
+        }
+        if (!this.coeffSexe.getText().matches("^[1-9]{0,1}+$|^$")) {
+            this.coeffSexe.setText("");
+        }
+        if (!this.coeffHistory.getText().matches("^[1-9]{0,1}+$|^$")) {
+            this.coeffHistory.setText("");
+        }
+        if (!this.coeffHobbies.getText().matches("^[1-9]{0,1}+$|^$")) {
+            this.coeffHobbies.setText("");
+        }
     }
 }
--- dev/src/graphes/AffectationUtil.java
+++ dev/src/graphes/AffectationUtil.java
@@ -13,7 +13,37 @@ import voyages.*;
  */
 public class AffectationUtil {
 
-	static final int DEFAULT_WEIGHT = 1;
+	private static final int DEFAULT_WEIGHT = 1;
+	private static int coefficientHistory = 1;
+	private static int coefficientAge = 1;
+	private static int coefficientGender = 1;
+	private static int coefficientHobbies = 1;
+	private static int coefficientAnimalAllergy = 5;
+	private static int coefficientDiet = 5;
+
+	public static void setCoefficientHistory(int coefficientHistory) {
+		AffectationUtil.coefficientHistory = coefficientHistory;
+	}
+
+	public static void setCoefficientAge(int coefficientAge) {
+		AffectationUtil.coefficientAge = coefficientAge;
+	}
+
+	public static void setCoefficientGender(int coefficientGender) {
+		AffectationUtil.coefficientGender = coefficientGender;
+	}
+
+	public static void setCoefficientHobbies(int coefficientHobbies) {
+		AffectationUtil.coefficientHobbies = coefficientHobbies;
+	}
+
+	public static void setCoefficientAnimalAllergy(int coefficientAnimalAllergy) {
+		AffectationUtil.coefficientAnimalAllergy = coefficientAnimalAllergy;
+	}
+
+	public static void setCoefficientDiet(int coefficientDiet) {
+		AffectationUtil.coefficientDiet = coefficientDiet;
+	}
 	
 	/** This method return the weight of the edge between the host and the visitor, based on their affinity.
 	 * @param host the {@code Teenager} host.
@@ -23,26 +53,26 @@ public class AffectationUtil {
 	public static double weight (Teenager host, Teenager visitor, List<Tuple<Teenager>> history) {
 		double weight = DEFAULT_WEIGHT;
 		if (host.compatibleWithGuestAnimalAllergy(visitor)) {
-			weight -= 0.5;
+			weight -= 0.1 * coefficientAnimalAllergy;
 		} else {
-			weight += 0.5;
+			weight += 0.1 * coefficientAnimalAllergy;
 		}
 		if (host.compatibleWithGuestDiet(visitor)) {
-			weight -= 0.5;
+			weight -= 0.1 * coefficientDiet;
 		} else {
-			weight += 0.5;
+			weight += 0.1 * coefficientDiet;
 		}
 		weight += historyWeight(host, visitor, history);
-		weight += host.totalCompatibleHobbies(visitor) * -0.1;
-		weight += ageWeight(host, visitor);
-		weight += genderWeight(host, visitor);
+		weight += host.totalCompatibleHobbies(visitor) * -0.1 * coefficientHobbies;
+		weight += ageWeight(host, visitor) * coefficientAge;
+		weight += genderWeight(host, visitor) * coefficientGender;
 		return weight;
 	}
 
 	private static double genderWeight(Teenager host, Teenager visitor) {
 		try {
 			if (host.getRequirement().get(CriterionName.PAIR_GENDER).equals(visitor.getRequirement().get(CriterionName.GENDER)) && visitor.getRequirement().get(CriterionName.PAIR_GENDER).equals(host.getRequirement().get(CriterionName.GENDER))) {
-				return -0.5;
+				return -0.1;
 			} else if (host.getRequirement().get(CriterionName.PAIR_GENDER).equals(visitor.getRequirement().get(CriterionName.GENDER)) || visitor.getRequirement().get(CriterionName.PAIR_GENDER).equals(host.getRequirement().get(CriterionName.GENDER))) {
 				return -0.1;
 			}
@@ -88,10 +118,10 @@ public class AffectationUtil {
 		else if (host.getRequirement().get(CriterionName.HISTORY).getValue().equals("same") || visitor.getRequirement().get(CriterionName.HISTORY).getValue().equals("same")) {
 			for (Tuple<Teenager> tuple : history) {
 				try {
-					if (tuple.get(host).equals(visitor)) return -0.1;
+					if (tuple.get(host).equals(visitor)) return -0.1 * coefficientHistory;
 				} catch (NullPointerException e1) {
 					try {
-						if (tuple.get(visitor).equals(host)) return -0.1;
+						if (tuple.get(visitor).equals(host)) return -0.1 * coefficientHistory;
 					} catch (NullPointerException e2) {
 						continue;
 					}
