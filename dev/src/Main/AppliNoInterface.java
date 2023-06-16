819a21e364cc1f353057572fb648ae46d756db85
alexandre.martel.etu <alexandre.martel.etu@univ-lille.fr>
2023-06-16 19:11:35 +0200

    Fix ask function et ajout coeffChange

--- dev/src/Main/AppliNoInterface.java
+++ dev/src/Main/AppliNoInterface.java
@@ -2,6 +2,7 @@ package Main;
 import java.io.File;
 import java.util.Scanner;
 
+import graphes.AffectationUtil;
 import voyages.CountryName;
 import voyages.Teenager;
 import voyages.Tuple;
@@ -9,7 +10,9 @@ import voyages.CsvFileImportator;
 import voyages.Platform;
 
 public class AppliNoInterface {
-    
+    public static Scanner read = new Scanner(System.in);
+    public static Tuple<CountryName> assoPays = new Tuple<>();
+
     private static void mainMenu() {
         //Déclaration des varibes utilisées plus loin
         Platform p = new Platform();
@@ -27,15 +30,11 @@ public class AppliNoInterface {
 				isFileOk = true;
 			}
 		} while (!isFileOk);
-        
 
         System.out.println("Importation terminée\n");
 
         System.out.println("Vous allez maintenant pouvoir sélectionner deux pays avec lesquels vous souhaitez faire les associations d'étudiants");
-        Tuple<CountryName> assoPays =  askPays();
-
-        askForSomething(assoPays,p);
-
+        askPays();
     }
 
     public static File askChemin(){
@@ -44,8 +43,8 @@ public class AppliNoInterface {
         File result = null;
         boolean stop = false;
         do {
-        	Scanner reponse = new Scanner(System.in);
-        	chemin = reponse.nextLine();
+        	Scanner read = new Scanner(System.in);
+        	chemin = read.nextLine();
         	if (!chemin.contains(".csv")) {
         		System.out.println("Le chemin ne mène pas vers un fichier csv");
                 System.out.println("Veuillez saisir un chemin correct");
@@ -56,25 +55,8 @@ public class AppliNoInterface {
 				} else {
 					stop = true;
 				}
-			}/*
-        	try (Scanner reponse = new Scanner(System.in)) {
-            	chemin = reponse.nextLine();
-            	if (!chemin.contains(".csv")) {
-            		System.out.println("Le chemin ne mène pas vers un fichier csv");
-                    System.out.println("Veuillez saisir un chemin correct");
-				} else {
-					result = new File(chemin);
-					if (!result.exists()) {
-						System.out.println("Le fichier n'existe pas, veuillez recommencer...");
-					} else {
-						stop = true;
-					}
-				}
-    		} catch (Exception e) {
-    			e.printStackTrace();
-    		}*/
+			}
 		} while (!stop);
-        System.out.print('\u000C');
         return result;
     }
 
@@ -86,68 +68,62 @@ public class AppliNoInterface {
         System.out.println("4. Modifier les coefficients d'affectations");
         System.out.println("5. Modifier les deux pays d'échanges");
         System.out.println("6. Quitter l'application");
-
-        try (Scanner reponse = new Scanner(System.in)) {
-            String read;
-            do{
-                read = reponse.nextLine();
-                if(read.equals("1")){
-                    associationForced();
-                }
-                else if(read.equals("2")){
-                    associationForbidden();
-                }
-                else if(read.equals("3")){
-                    associationAuto();
-                }
-                else if(read.equals("4")){
-                    coeffChange();
-                }
-                else if(read.equals("5")){
-                    askPays();
-                }
-                else if(read.equals("6")){
-                    break;
-                }
-                else{
-                    System.out.println("Veuillez rentrer une réponse valide");
-                }
-
-            }while(!(read.equals("1"))||read.equals("2")||read.equals("3")||read.equals("4")||read.equals("5")||read.equals("6"));
-        }
+        
+        String reponse;
+        do{
+            reponse = read.nextLine();
+            if(reponse.equals("1")){
+                associationForced();
+            }
+            else if(reponse.equals("2")){
+                associationForbidden();
+            }
+            else if(reponse.equals("3")){
+                associationAuto();
+            }
+            else if(reponse.equals("4")){
+                coeffChange(assoPays,p);
+            }
+            else if(reponse.equals("5")){
+                askPays();
+            }
+            else if(reponse.equals("6")){
+                break;
+            }
+            else{
+                System.out.println("Veuillez rentrer une réponse valide");
+            }
+        }while(!(reponse.equals("1"))||reponse.equals("2")||reponse.equals("3")||reponse.equals("4")||reponse.equals("5")||reponse.equals("6"));
     }
 
-    public static Tuple<CountryName> askPays(){
-        Tuple<CountryName> assoPays = new Tuple<>();
-
+    public static void askPays(){
         System.out.println("Sélectionner Pays hôte :");
         System.out.println("1. France");
         System.out.println("2. Italy");
         System.out.println("3. Spain");
         System.out.println("4. Germany");
 
-        try (Scanner hote = new Scanner(System.in)) {
-            String readHote;
-            do{
-                readHote = hote.nextLine();
-                if(readHote.equals("1")){
-                    assoPays.setFirst(CountryName.FRANCE);
-                }
-                else if(readHote.equals("2")){
-                    assoPays.setFirst(CountryName.ITALY);
-                }
-                else if(readHote.equals("3")){
-                    assoPays.setFirst(CountryName.SPAIN);
-                }
-                else if(readHote.equals("4")){
-                    assoPays.setFirst(CountryName.GERMANY);
-                }
-                else{
-                    System.out.println("Veuillez rentrer une réponse valide");
-                }
+        String readHote;
+        do{
+            readHote = read.nextLine();
+            if(readHote.equals("1")){
+                assoPays.setFirst(CountryName.FRANCE);
+            }
+            else if(readHote.equals("2")){
+                assoPays.setFirst(CountryName.ITALY);
+            }
+            else if(readHote.equals("3")){
+                assoPays.setFirst(CountryName.SPAIN);
+            }
+            else if(readHote.equals("4")){
+                assoPays.setFirst(CountryName.GERMANY);
+            }
+            else{
+                System.out.println("Veuillez rentrer une réponse valide");
+            }
+        }while(!((readHote.equals("1"))||readHote.equals("2")||readHote.equals("3")||readHote.equals("4")));
 
-            }while(!(readHote.equals("1"))||readHote.equals("2")||readHote.equals("3")||readHote.equals("4"));
-        }
+        System.out.println(assoPays.getFirst());
         
         System.out.println("Sélectionner Pays visiteur:");
         System.out.println("1. France");
@@ -155,33 +131,31 @@ public class AppliNoInterface {
         System.out.println("3. Spain");
         System.out.println("4. Germany");
 
-        try (Scanner visiteur = new Scanner(System.in)) {
-            String readVisiteur;
-            do{
-                readVisiteur = visiteur.nextLine();
-                if(readVisiteur.equals("1")){
-                    assoPays.setSecond(CountryName.FRANCE);
-                }
-                else if(readVisiteur.equals("2")){
-                    assoPays.setSecond(CountryName.ITALY);
-                }
-                else if(readVisiteur.equals("3")){
-                    assoPays.setSecond(CountryName.SPAIN);
-                }
-                else if(readVisiteur.equals("4")){
-                    assoPays.setSecond(CountryName.GERMANY);
-                }
-                else{
-                    System.out.println("Veuillez rentrer une réponse valide");
-                }
+        String readVisiteur;
+        do{
+            if(assoPays.getFirst().equals(assoPays.getSecond())){
+                System.out.println("Attention vous ne pouvez pas associer deux fois le même pays !");
+            }
+            readVisiteur = read.nextLine();
+            if(readVisiteur.equals("1")){
+                assoPays.setSecond(CountryName.FRANCE);
+            }
+            else if(readVisiteur.equals("2")){
+                assoPays.setSecond(CountryName.ITALY);
+            }
+            else if(readVisiteur.equals("3")){
+                assoPays.setSecond(CountryName.SPAIN);
+            }
+            else if(readVisiteur.equals("4")){
+                assoPays.setSecond(CountryName.GERMANY);
+            }
+            else{
+                System.out.println("Veuillez rentrer une réponse valide");
+            }
 
-                if(assoPays.getFirst().equals(assoPays.getSecond())){
-                    System.out.println("Attention vous ne pouvez pas associer deux fois le même pays");
-                }
-
-            }while(!(readVisiteur.equals("1"))||readVisiteur.equals("2")||readVisiteur.equals("3")||readVisiteur.equals("4")||assoPays.getFirst().equals(assoPays.getSecond()));
-        }
-            return assoPays;
+        }while(!((readVisiteur.equals("1"))||readVisiteur.equals("2")||readVisiteur.equals("3")||readVisiteur.equals("4"))||assoPays.getFirst().equals(assoPays.getSecond()));
+        System.out.println("Envoi vers le menu de selection d'action");
+        askForSomething(assoPays, null);
     }
     
 
@@ -194,10 +168,67 @@ public class AppliNoInterface {
     }
 
     public static void associationAuto(){
+
     }
 
-    public static void coeffChange(){
+    public static void coeffChange(Tuple<CountryName> assoPays, Platform p){
+        System.out.println("Dans cette partie vous pouvez modifier les coefficients du calculateur qui permet de faire des associations d'étudiants de manière automatique");
+        System.out.println("Quel coefficient souhaitez-vous modifier ?\n");
+        System.out.println("1. Coefficient de l'historique");
+        System.out.println("2. Coefficient de l'âge");
+        System.out.println("3. Coefficient du genre");
+        System.out.println("4. Coefficient des hobbies");
+        System.out.println("5. Coefficient des allergies aux animaux");
+        System.out.println("6. Coefficient des contraintes d'alimentation");
+        System.out.println("7. Retour au menu de sélection d'action");
+        System.out.println("8. Quitter l'application");
 
+        String coeffNb;
+        String newValue;
+        do{
+            coeffNb = read.nextLine();
+            if(coeffNb.equals("1")){
+                System.out.println("Veuillez entrer la nouvelle valeur souhaitée : (la valeur doit être entière)");
+                newValue = read.nextLine();
+                AffectationUtil.setCoefficientHistory(Integer.parseInt(newValue));
+            }
+            else if(coeffNb.equals("2")){
+                System.out.println("Veuillez entrer la nouvelle valeur souhaitée : (la valeur doit être entière)");
+                newValue = read.nextLine();
+                AffectationUtil.setCoefficientAge(Integer.parseInt(newValue));
+            }
+            else if(coeffNb.equals("3")){
+                System.out.println("Veuillez entrer la nouvelle valeur souhaitée : (la valeur doit être entière)");
+                newValue = read.nextLine();
+                AffectationUtil.setCoefficientGender(Integer.parseInt(newValue));
+            }
+            else if(coeffNb.equals("4")){
+                System.out.println("Veuillez entrer la nouvelle valeur souhaitée : (la valeur doit être entière)");
+                newValue = read.nextLine();
+                AffectationUtil.setCoefficientHobbies(Integer.parseInt(newValue));
+            }
+            else if(coeffNb.equals("5")){
+                System.out.println("Veuillez entrer la nouvelle valeur souhaitée : (la valeur doit être entière)");
+                newValue = read.nextLine();
+                AffectationUtil.setCoefficientAnimalAllergy(Integer.parseInt(newValue));
+            }
+            else if(coeffNb.equals("6")){
+                System.out.println("Veuillez entrer la nouvelle valeur souhaitée : (la valeur doit être entière)");
+                newValue = read.nextLine();
+                AffectationUtil.setCoefficientDiet(Integer.parseInt(newValue));
+            }
+            else if(coeffNb.equals("7")){
+                askForSomething(assoPays,p);
+            }
+            else if(coeffNb.equals("8")){
+                break;
+            }
+            else{
+                System.out.println("Veuillez rentrer une réponse valide");
+            }
+        }while(!((coeffNb.equals("1"))||coeffNb.equals("2")||coeffNb.equals("3")||coeffNb.equals("4")||coeffNb.equals("5")||coeffNb.equals("6")||coeffNb.equals("7")||coeffNb.equals("8")));
+        System.out.println("Valeur modifiée ! Retour au menu principal");
+        askForSomething(assoPays, p);
     }
     
     
@@ -218,7 +249,6 @@ public class AppliNoInterface {
             do{
                 read = reponse.nextLine();
                 if(read.toLowerCase().equals("oui")){
-                    System.out.print('\u000C');
                     mainMenu();
                 }
                 else if(read.toLowerCase().equals("non")){
