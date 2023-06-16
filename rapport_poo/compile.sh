cd920a7e89cc43c3977e9b857727922ca67c7184
Paul <paul1.degraeve@gmail.com>
2023-06-16 20:53:50 +0200

    début rapport poo

--- rapport_poo/compile.sh
+++ rapport_poo/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Génération du rapport HTML
-pandoc Rapport.md --syntax-definition=theme/java.xml --pdf-engine=prince --css=styles/style-pdf.css --toc --toc-depth=4 --section-divs -s -o Compte-rendu-IHM.html
+pandoc Rapport.md --syntax-definition=theme/java.xml --pdf-engine=prince --css=styles/style-pdf.css --toc --toc-depth=4 --section-divs -s -o Compte-rendu-POO.html
 
 #Génération du rapport PDF en utilisant le moteur "prince"
-pandoc Rapport.md --syntax-definition=theme/java.xml --pdf-engine=prince --css=styles/style-pdf.css --toc --toc-depth=4 --section-divs -s -o Compte-rendu-IHM.pdf
+pandoc Rapport.md --syntax-definition=theme/java.xml --pdf-engine=prince --css=styles/style-pdf.css --toc --toc-depth=4 --section-divs -s -o Compte-rendu-POO.pdf
